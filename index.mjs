ed430c9d4c2d8f585f2b42788bed44a22c9cf825
Onelight <100507149+OneLightWebDev@users.noreply.github.com>
2023-09-23 16:16:55 -0700

    Add files via upload

---
+++ index.mjs
@@ -0,0 +1,62 @@
+import {
+  S3Client,
+  GetObjectCommand,
+  PutObjectCommand,
+} from "@aws-sdk/client-s3";
+import sharp from "sharp";
+
+const S3 = new S3Client();
+const DEST_BUCKET = "nikita-thumbnails";
+const THUMBNAIL_WIDTH = 200; // px
+const SUPPORTED_FORMATS = {
+  jpg: true,
+  jpeg: true,
+  png: true,
+};
+
+export const handler = async (event, context) => {
+  const { eventTime, s3 } = event.Records[0];
+  const srcBucket = s3.bucket.name;
+
+  // Object key may have spaces or unicode non-ASCII characters
+  const srcKey = decodeURIComponent(s3.object.key.replace(/\+/g, " "));
+  const ext = srcKey.replace(/^.*\./, "").toLowerCase();
+
+  console.log(`${eventTime} - ${srcBucket}/${srcKey}`);
+
+  if (!SUPPORTED_FORMATS[ext]) {
+    console.log(`ERROR: Unsupported file type (${ext})`);
+    return;
+  }
+
+  // Get the image from the source bucket
+  try {
+    const { Body, ContentType } = await S3.send(
+      new GetObjectCommand({
+        Bucket: srcBucket,
+        Key: srcKey,
+      })
+    );
+    const image = await Body.transformToByteArray();
+    // resize image
+    const outputBuffer = await sharp(image).resize(THUMBNAIL_WIDTH).toBuffer();
+
+    // store new image in the destination bucket
+    await S3.send(
+      new PutObjectCommand({
+        Bucket: DEST_BUCKET,
+        Key: srcKey,
+        Body: outputBuffer,
+        ContentType,
+      })
+    );
+    const message = `Successfully resized ${srcBucket}/${srcKey} and uploaded to ${DEST_BUCKET}/${srcKey}`;
+    console.log(message);
+    return {
+      statusCode: 200,
+      body: message,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+};
